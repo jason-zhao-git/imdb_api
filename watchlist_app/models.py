8209f29906e96a26c6f8445593a5fd36342a3b7e
jason-zhao-git <zjq233333@gmail.com>
2024-08-10 01:16:55 -0700

    first commit

---
+++ watchlist_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length=30)
+    about = models.CharField(max_length=150)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
